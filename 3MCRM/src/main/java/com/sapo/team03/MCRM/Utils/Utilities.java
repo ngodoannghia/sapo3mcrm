e7150551bbe1e89717f6a45f5c154d72f566d7db
ngodoannghia <ngodoannghia07071998@gmail,com>
2019-07-23 17:16:38 +0700

    first commit

---
+++ 3MCRM/src/main/java/com/sapo/team03/MCRM/Utils/Utilities.java
@@ -0,0 +1,11 @@
+package com.sapo.team03.MCRM.Utils;
+
+import org.springframework.stereotype.Component;
+
+@Component("Utilities")
+public class Utilities {
+	public void log(String log) {
+		System.out.println(log);
+	}
+
+}
